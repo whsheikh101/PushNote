9a4d3a7e482c577c7971815bf690338addb43fe4
Khurram Iqbal <khurram.iqbal@veripark.com>
2020-02-26 02:35:08 +0500

    Delete View Controller Push Icon on Index

---
+++ PushNote/ViewControllers/DeleteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DeleteViewController.swift
+//  PushNote
+//
+//  Created by Khurram Iqbal on 26/02/2020.
+//  Copyright © 2020 Ihsan Bhatti. All rights reserved.
+//
+
+import UIKit
+
+class DeleteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
